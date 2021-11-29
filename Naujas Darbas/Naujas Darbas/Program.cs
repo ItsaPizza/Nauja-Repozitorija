91d0cf9ba5f783a6c47eca853b7dbddeaadd7fc6
Laurynas <laurynas.balsys91@gmail.com>
2021-11-29 21:47:49 +0200

    Skaiciavimai

--- Naujas Darbas/Naujas Darbas/Program.cs
+++ Naujas Darbas/Naujas Darbas/Program.cs
@@ -11,7 +11,9 @@ namespace Naujas_Darbas
         static void Main(string[] args)
         {
 
-            Console.WriteLine("New");
+            double zemesPlotas = 4 * 3.14 * 6371 * 6371;
+            double izambine = Math.Sqrt(10 * 10 + 15 * 15);
+
 
         }
     }
