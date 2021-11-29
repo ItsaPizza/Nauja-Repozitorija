9f087ba894cf83df5c36cfd7e3c89a5627e31477
Kuodys <vaidotas.kuodys@gmail.com>
2021-11-29 21:47:55 +0200

    Isspresto uzduotys

--- Naujas Darbas/Naujas Darbas/Program.cs
+++ Naujas Darbas/Naujas Darbas/Program.cs
@@ -11,7 +11,25 @@ namespace Naujas_Darbas
         static void Main(string[] args)
         {
 
-            Console.WriteLine("New");
+            double zemesPlotas=0;
+            double zemesTuris=0;
+            double izambine=0;
+            double suma=0;
+            double vidurkis=0;
+
+            zemesTuris = 4 / 3 * 3.14 * 6371 * 6371 * 6371;
+
+            suma= zemesPlotas + zemesTuris+ izambine;
+
+            vidurkis = (zemesPlotas + zemesTuris + izambine) / 3;
+
+            Console.WriteLine($"Viso kintamuju: 3!");
+            Console.WriteLine($"Zemes plotas:{zemesPlotas}!");
+            Console.WriteLine($"Zemes turis: {zemesTuris}!");
+            Console.WriteLine($"Izamabine: {izambine}!");
+            Console.WriteLine($"Suma:{suma}!");
+            Console.WriteLine($"Vidurkis:{vidurkis}!");
+            
 
         }
     }
